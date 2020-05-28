f564f2bba78d9ac11cbd1099cddbfe62b3b2d217
Mark <36554048+xmarkas@users.noreply.github.com>
2020-05-28 01:07:49 -0700

    Add files via upload

---
+++ src/store/action-types.js
@@ -0,0 +1,22 @@
+// UI
+export const ADD_ARTICLE = "ADD_ARTICLE";
+export const FLIP_DISPLAY = "FLIP_DISPLAY";
+export const DIALPAD_SHOW = "DIALPAD_SHOW";
+export const ACCOUNT_SHOW = "ACCOUNT_SHOW";
+export const ADD_CONTACT = "ADD_CONTACT";
+export const EDIT_CONTACT = "EDIT_CONTACT";
+export const TOGGLE_FAVORITE = "TOGGLE_FAVORITE";
+export const POP_MESSAGE = "POP_MESSAGE";
+
+// Plivo
+export const SET_USER_ID = "SET_USER_ID";
+
+// Calls
+export const ADD_CALL = "ADD_CALL";
+export const UPDATE_STACK = "UPDATE_STACK";
+export const MAKE_DISPLAY_CALL = "MAKE_DISPLAY_CALL";
+export const CALL_SWITCH = "CALL_SWITCH";
+export const SET_CLAN_CALL = "SET_CLAN_CALL";
+
+
+
